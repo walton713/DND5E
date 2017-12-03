954f1903266e46656542602a5762eecfd63a0b01
Ben Walton <walton713@gmail.com>
2017-12-03 06:38:50 -0600

    Created DiceRoller Feature Tests

---
+++ DND5EUnitTests/DiceRoller.feature
@@ -0,0 +1,13 @@
+﻿Feature: DiceRoller
+	As a user
+	I want a dice roller
+	So that I can play DND
+
+Scenario Outline: ReturnNumberBetweenOneAndSize
+	Given I have a Dice Roller
+	When I roll a dice with <size> sides
+	Then it returns a number between 1 and <size>
+
+	Examples: 
+	| size |
+	| 4    |
---
+++ DND5EUnitTests/DiceRollerSteps.cs
@@ -0,0 +1,26 @@
+﻿using TechTalk.SpecFlow;
+
+namespace DND5EUnitTests
+{
+    [Binding]
+    public class DiceRollerSteps
+    {
+        [Given(@"I have a Dice Roller")]
+        public void GivenIHaveADiceRoller()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I roll a dice with (.*) sides")]
+        public void WhenIRollADiceWithSides(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"it returns a number between (.*) and (.*)")]
+        public void ThenItReturnsANumberBetweenAnd(int p0, int p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
