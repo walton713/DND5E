3885efcfb40f58eb5a1f3a3d000bafedae55e205
Ben Walton <walton713@gmail.com>
2017-12-03 06:10:19 -0600

    Added Unit Test Project

--- DND5E/Main.Designer.cs
+++ DND5E/Main.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace DND5E
 {
-    partial class Form1
+    partial class Main
     {
         /// <summary>
         /// Required designer variable.
--- DND5E/Program.cs
+++ DND5E/Program.cs
@@ -16,7 +16,7 @@ namespace DND5E
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Main());
         }
     }
 }
